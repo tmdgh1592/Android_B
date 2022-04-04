7e1f06c333e1914a7472b4d75fb3533bfa5aebae
tmdgh1592 <tmdgh1592@naver.com>
2022-04-04 13:13:30 +0900

    UMC 3주차 클론 코딩

--- app/src/main/java/com/umc/clone_flo/LockerFragment.kt
+++ app/src/main/java/com/umc/clone_flo/LockerFragment.kt
@@ -24,7 +24,6 @@ class LockerFragment : Fragment() {
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
